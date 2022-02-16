a92c842909edded4bef6639ba13cb34743b4e10a
Mauricio Gutierrez <developer.mauricio2310@gmail.com>
2022-02-16 10:00:08 -0500

    ajustes

--- wp-content/uploads/custom-css-js/8261.js
+++ wp-content/uploads/custom-css-js/8261.js
@@ -14,5 +14,61 @@ function scrollBottom( idElement ) {
 }
 
 
-</script>
+
+
+const urlImg = "https://visual2etex.com/wp-content/uploads/2021/12/";
+const plane1And7 = document.querySelectorAll(".model-plane-one");
+for (i = 0; i < plane1And7.length; i++) {
+  plane1And7[i].onclick = () => { openInfoPlane(urlImg + "plano-apartamento-1.png") };
+}
+
+const planeTwo = document.querySelectorAll(".model-plane-two");
+for (i = 0; i < planeTwo.length; i++) {
+  planeTwo[i].onclick = () => { openInfoPlane(urlImg + "plano-apartamento-17-23-78.png") };
+}
+
+const planeThree = document.querySelectorAll(".model-plane-three");
+for (i = 0; i < planeThree.length; i++) {
+  planeThree[i].onclick = () => { openInfoPlane(urlImg + "plano-apartamento-51-77-88.png") };
+}
+
+const planeFour = document.querySelectorAll(".model-plane-four");
+for (i = 0; i < planeFour.length; i++) {
+  planeFour[i].onclick = () => { openInfoPlane(urlImg + "plano-apartamento-90-91.png") };
+}
+
+const planeFive = document.querySelectorAll(".model-plane-five");
+for (i = 0; i < planeFive.length; i++) {
+  planeFive[i].onclick = () => { openInfoPlane(urlImg + "plano-apartamento-94-171.png") };
+}
+
+document.querySelector(".model-plane-8").addEventListener("click", () => { openInfoPlane(urlImg + "plano-apartamento-8.png") }, false);
+document.querySelector(".model-plane-11").addEventListener("click", () => { openInfoPlane(urlImg + "plano-apartamento-11.png") }, false);
+document.querySelector(".model-plane-12").addEventListener("click", () => { openInfoPlane(urlImg + "plano-apartamento-12.png") }, false);
+document.querySelector(".model-plane-61").addEventListener("click", () => { openInfoPlane(urlImg + "plano-apartamento-61.png") }, false);
+document.querySelector(".model-plane-76").addEventListener("click", () => { openInfoPlane(urlImg + "plano-apartamento-76.png") }, false);
+document.querySelector(".model-plane-89").addEventListener("click", () => { openInfoPlane(urlImg + "plano-apartamento-89.png") }, false);
+document.querySelector(".model-plane-99").addEventListener("click", () => { openInfoPlane(urlImg + "plano-apartamento-99.png") }, false);
+document.querySelector(".model-plane-100").addEventListener("click", () => { openInfoPlane(urlImg + "plano-apartamento-100.png") }, false);
+
+
+const openInfoPlane = ( urlImgPlane ) => {
+  document.getElementById("mainContentPlane").style.display = "flex";
+  document.getElementById("mostrarPlano").src = urlImgPlane;
+}
+
+
+document.querySelector("#goApartament").addEventListener("click", function() {
+  location.href = '/apartamento-escandinavo/';
+}, false);
+
+document.querySelector("#closeContentPlane").addEventListener("click", function() {
+  document.getElementById("mainContentPlane").style.display = "none";
+  document.getElementById("mostrarPlano").src = "";
+}, false);
+
+document.querySelector("#mainContentPlane").addEventListener("click", function() {
+  document.getElementById("mainContentPlane").style.display = "none";
+  document.getElementById("mostrarPlano").src = "";
+}, false);</script>
 <!-- end Simple Custom CSS and JS -->
---
+++ wp-content/uploads/custom-css-js/9457.js
@@ -0,0 +1,61 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+const getTime = dateTo => {
+    let now = new Date(),
+        time = (new Date(dateTo) - now + 1000) / 1000,
+        seconds = ('0' + Math.floor(time % 60)).slice(-2),
+        minutes = ('0' + Math.floor(time / 60 % 60)).slice(-2),
+        hours = ('0' + Math.floor(time / 3600 % 24)).slice(-2),
+        days = Math.floor(time / (3600 * 24));
+
+    return {
+        seconds,
+        minutes,
+        hours,
+        days,
+        time
+    }
+};
+
+const elementHTLM = (day, hour, minute, currenTime) => {
+  day.innerHTML = currenTime.days;
+  hour.innerHTML = currenTime.hours;
+  minute.innerHTML = currenTime.minutes;  
+}
+
+const countdown = (dateTo) => {
+  
+  const MyDay = document.querySelector('#MyDay');
+  const MyHour = document.querySelector('#MyHour');
+  const MyMinute = document.querySelector('#MyMinute');
+ 
+  let currenTime = getTime(dateTo);
+  if (currenTime.time < 1) {
+    MyDay.innerHTML = '00';
+    MyHour.innerHTML = '00';
+    MyMinute.innerHTML = '00';
+    return;
+  }
+  elementHTLM( MyDay, MyHour, MyMinute, currenTime );
+  
+  const timerUpdate = setInterval( () => {
+    currenTime = getTime(dateTo);
+    elementHTLM( MyDay, MyHour, MyMinute, currenTime );
+
+    if (currenTime.time < 1) {
+      clearInterval(timerUpdate);
+      MyDay.innerHTML = '00';
+      MyHour.innerHTML = '00';
+      MyMinute.innerHTML = '00';  
+    }
+  }, 60000);
+}
+
+
+setTimeout( () => {
+  countdown( new Date(2022, 1, 28, 8,0,0) );
+}, 3000);
+
+
+</script>
+<!-- end Simple Custom CSS and JS -->
